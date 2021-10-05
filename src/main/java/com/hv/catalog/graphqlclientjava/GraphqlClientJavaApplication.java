c5ef9c90a0ad8da0f20650f6dc99c55a1704262b
Prasanth <60735013+rkprasanth@users.noreply.github.com>
2021-10-05 21:13:07 +0530

    [GraphQL] Graphql client on Java

---
+++ src/main/java/com/hv/catalog/graphqlclientjava/GraphqlClientJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hv.catalog.graphqlclientjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlClientJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlClientJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hv/catalog/graphqlclientjava/GraphqlClientJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hv.catalog.graphqlclientjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlClientJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
